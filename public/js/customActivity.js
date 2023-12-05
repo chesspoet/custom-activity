07c66dd0f0e377e670e6888c716f7522d623c1e6
chesspoet <oswrher@gmail.com>
2023-12-05 17:02:54 -0600

    buenocommit
    qweqw

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -6,36 +6,52 @@ define([
     'use strict';
 
     var connection = new Postmonger.Session();
+    var authTokens = {};
     var payload = {};
-    var lastStepEnabled = false;
-    var steps = [ // initialize to the same value as what's set in config.json for consistency
-        { "label": "Create SMS Message", "key": "step1" }
-    ];
-    var currentStep = steps[0].key;
-
     $(window).ready(onRender);
 
     connection.on('initActivity', initialize);
     connection.on('requestedTokens', onGetTokens);
     connection.on('requestedEndpoints', onGetEndpoints);
+    connection.on('requestedInteraction', onRequestedInteraction);
+    connection.on('requestedTriggerEventDefinition', onRequestedTriggerEventDefinition);
+    connection.on('requestedDataSources', onRequestedDataSources);
 
     connection.on('clickedNext', save);
-    //connection.on('clickedBack', onClickedBack);
-    //connection.on('gotoStep', onGotoStep);
-
+   
     function onRender() {
         // JB will respond the first time 'ready' is called with 'initActivity'
         connection.trigger('ready');
+
         connection.trigger('requestTokens');
         connection.trigger('requestEndpoints');
+        connection.trigger('requestInteraction');
+        connection.trigger('requestTriggerEventDefinition');
+        connection.trigger('requestDataSources');  
+
+    }
+
+    function onRequestedDataSources(dataSources){
+        console.log('*** requestedDataSources ***');
+        console.log(dataSources);
+    }
+
+    function onRequestedInteraction (interaction) {    
+        console.log('*** requestedInteraction ***');
+        console.log(interaction);
+     }
+
+     function onRequestedTriggerEventDefinition(eventDefinitionModel) {
+        console.log('*** requestedTriggerEventDefinition ***');
+        console.log(eventDefinitionModel);
     }
 
     function initialize(data) {
-        console.log("Initializing data data: " + JSON.stringify(data));
+        console.log(data);
         if (data) {
             payload = data;
         }
-
+        
         var hasInArguments = Boolean(
             payload['arguments'] &&
             payload['arguments'].execute &&
@@ -45,65 +61,44 @@ define([
 
         var inArguments = hasInArguments ? payload['arguments'].execute.inArguments : {};
 
-        console.log('Has In arguments: ' + JSON.stringify(inArguments));
+        console.log(inArguments);
+
+        $.each(inArguments, function (index, inArgument) {
+            $.each(inArgument, function (key, val) {
+                
+              
+            });
+        });
 
         connection.trigger('updateButton', {
             button: 'next',
             text: 'done',
             visible: true
         });
-
     }
 
     function onGetTokens(tokens) {
-        // Response: tokens = { token: <legacy token>, fuel2token: <fuel api token> }
-        console.log("Tokens function: " + JSON.stringify(tokens));
-        //authTokens = tokens;
+        console.log(tokens);
+        authTokens = tokens;
     }
 
     function onGetEndpoints(endpoints) {
-        // Response: endpoints = { restHost: <url> } i.e. "rest.s1.qa1.exacttarget.com"
-        console.log("Get End Points function: " + JSON.stringify(endpoints));
+        console.log(endpoints);
     }
 
     function save() {
-        var myHeaders = new Headers();
-        myHeaders.append("Content-Type", "application/json");
-        myHeaders.append("Authorization", "Bearer 5b12b8d9-20b1-1b49-762d-5ca346e02445");
+        var postcardURLValue = $('#postcard-url').val();
+        var postcardTextValue = $('#postcard-text').val();
 
-        var raw = JSON.stringify({
-            "templateId": "X1Mva7L7EVd06JeaeF8A",
-            "phoneNumber": "522811071722",
-            "clientName": "Isabel De Gante",
-            "groupName": "Purdy Citas",
-            "assign": false,
-            "params": {
-                "Unidad": "010203",
-                "Vehiculo": "BB61 - COASTER HIGH LINE 4.0L TM 28 PAX T.ALTO",
-                "client_name": "David Espinoza Espinoza"
-            }
-        });
-
-        var requestOptions = {
-            method: 'POST',
-            headers: myHeaders,
-            body: raw,
-            redirect: 'follow'
-        };
-
-        fetch("https://us-central1-atomchat-io.cloudfunctions.net/templates", requestOptions)
-            .then(response => response.text())
-            .then(result => console.log(result))
-            .catch(error => console.log('error', error));
         payload['arguments'].execute.inArguments = [{
-            "phone": "{{Contact.Attribute.data_for_user_whats.phoneNumber}}" //<----This should map to your data extension name and phone number column
+            "tokens": authTokens
         }];
-
+        
         payload['metaData'].isConfigured = true;
 
-        console.log("Payload on SAVE function: " + JSON.stringify(payload));
+        console.log(payload);
         connection.trigger('updateActivity', payload);
-
     }
+
 
 });
--- routes/activity.js
+++ routes/activity.js
@@ -4,6 +4,7 @@ var util = require('util');
 // Deps
 const Path = require('path');
 const JWT = require(Path.join(__dirname, '..', 'lib', 'jwtDecoder.js'));
+var util = require('util');
 var http = require('https');
 
 exports.logExecuteData = [];
@@ -20,7 +21,7 @@ function logData(req) {
         route: req.route,
         cookies: req.cookies,
         ip: req.ip,
-        path: req.path, 
+        path: req.path,
         host: req.host,
         fresh: req.fresh,
         stale: req.stale,
@@ -51,14 +52,6 @@ function logData(req) {
  * POST Handler for / route of Activity (this is the edit route).
  */
 exports.edit = function (req, res) {
-
-    console.log("5 -- For Edit");	
-    console.log("4");	
-    console.log("3");	
-    console.log("2");	
-    console.log("1");	
-    //console.log("Edited: "+req.body.inArguments[0]);    
-    
     // Data from the req and put it in an array accessible to the main app.
     //console.log( req.body );
     logData(req);
@@ -69,16 +62,8 @@ exports.edit = function (req, res) {
  * POST Handler for /save/ route of Activity.
  */
 exports.save = function (req, res) {
-    
-    console.log("5 -- For Save");	
-    console.log("4");	
-    console.log("3");	
-    console.log("2");	
-    console.log("1");	
-    //console.log("Saved: "+req.body.inArguments[0]);
-    
     // Data from the req and put it in an array accessible to the main app.
-    console.log( req.body );
+    //console.log( req.body );
     logData(req);
     res.send(200, 'Save');
 };
@@ -88,59 +73,27 @@ exports.save = function (req, res) {
  */
 exports.execute = function (req, res) {
 
-    console.log("5 -- For Execute");	
-    console.log("4");	
-    console.log("3");	
-    console.log("2");	
-    console.log("1");	
-    //console.log("Executed: "+req.body.inArguments[0]);
-    
-    var requestBody = req.body.inArguments[0];
+    // example on how to decode JWT
+    JWT(req.body, process.env.jwtSecret, (err, decoded) => {
 
-    const accountSid = requestBody.accountSid;
-    const authToken = requestBody.authToken;
-    const to = requestBody.to;
-    const from = requestBody.messagingService;
-    const body = requestBody.body;;
+        // verification error -> unauthorized request
+        if (err) {
+            console.error(err);
+            return res.status(401).end();
+        }
 
-    const client = require('twilio')(accountSid, authToken); 
-     
-    client.messages 
-          .create({ 
-             body: body,
-             messagingService: messagingService,
-             to: to
-           }) 
-          .then(message => console.log(message.sid)) 
-          .done();
-
-
-
-    // FOR TESTING
-    logData(req);
-    res.send(200, 'Publish');
-
-    // Used to decode JWT
-    // JWT(req.body, process.env.jwtSecret, (err, decoded) => {
-
-    //     // verification error -> unauthorized request
-    //     if (err) {
-    //         console.error(err);
-    //         return res.status(401).end();
-    //     }
-
-    //     if (decoded && decoded.inArguments && decoded.inArguments.length > 0) {
+        if (decoded && decoded.inArguments && decoded.inArguments.length > 0) {
             
-    //         // decoded in arguments
-    //         var decodedArgs = decoded.inArguments[0];
+            // decoded in arguments
+            var decodedArgs = decoded.inArguments[0];
             
-    //         logData(req);
-    //         res.send(200, 'Execute');
-    //     } else {
-    //         console.error('inArguments invalid.');
-    //         return res.status(400).end();
-    //     }
-    // });
+            logData(req);
+            res.send(200, 'Execute');
+        } else {
+            console.error('inArguments invalid.');
+            return res.status(400).end();
+        }
+    });
 };
 
 
@@ -148,32 +101,16 @@ exports.execute = function (req, res) {
  * POST Handler for /publish/ route of Activity.
  */
 exports.publish = function (req, res) {
-
-    console.log("5 -- For Publish");	
-    console.log("4");	
-    console.log("3");	
-    console.log("2");	
-    console.log("1");	
-    //console.log("Published: "+req.body.inArguments[0]);        
-    
     // Data from the req and put it in an array accessible to the main app.
     //console.log( req.body );
-//     logData(req);
-//     res.send(200, 'Publish');
+    logData(req);
+    res.send(200, 'Publish');
 };
 
 /*
  * POST Handler for /validate/ route of Activity.
  */
 exports.validate = function (req, res) {
-
-    console.log("5 -- For Validate");	
-    console.log("4");	
-    console.log("3");	
-    console.log("2");	
-    console.log("1");	
-    //console.log("Validated: "+req.body.inArguments[0]);       
-    
     // Data from the req and put it in an array accessible to the main app.
     //console.log( req.body );
     logData(req);
