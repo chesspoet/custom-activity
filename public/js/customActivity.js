e5512091fef9920a41a8b6746675b2f4cdfc7578
chesspoet <oswrher@gmail.com>
2023-12-05 15:52:19 -0600

    Nuevocommit
    Es el bueno

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -1,6 +1,6 @@
 define([
     'postmonger'
-], function(
+], function (
     Postmonger
 ) {
     'use strict';
@@ -30,44 +30,22 @@ define([
         connection.trigger('requestEndpoints');
     }
 
-  function initialize(data) {
-        console.log("Initializing data data: "+ JSON.stringify(data));
+    function initialize(data) {
+        console.log("Initializing data data: " + JSON.stringify(data));
         if (data) {
             payload = data;
-        }    
+        }
 
         var hasInArguments = Boolean(
             payload['arguments'] &&
             payload['arguments'].execute &&
             payload['arguments'].execute.inArguments &&
             payload['arguments'].execute.inArguments.length > 0
-         );
+        );
 
         var inArguments = hasInArguments ? payload['arguments'].execute.inArguments : {};
 
-        console.log('Has In arguments: '+JSON.stringify(inArguments));
-
-        $.each(inArguments, function (index, inArgument) {
-            $.each(inArgument, function (key, val) {
-
-                if (key === 'accountSid') {
-                    $('#accountSID').val(val);
-                }
-
-                if (key === 'authToken') {
-                    $('#authToken').val(val);
-                }
-
-                if (key === 'messagingService') {
-                    $('#messagingService').val(val);
-                }
-
-                if (key === 'body') {
-                    $('#messageBody').val(val);
-                }                                                               
-
-            })
-        });
+        console.log('Has In arguments: ' + JSON.stringify(inArguments));
 
         connection.trigger('updateButton', {
             button: 'next',
@@ -77,37 +55,55 @@ define([
 
     }
 
-    function onGetTokens (tokens) {
+    function onGetTokens(tokens) {
         // Response: tokens = { token: <legacy token>, fuel2token: <fuel api token> }
-        console.log("Tokens function: "+JSON.stringify(tokens));
+        console.log("Tokens function: " + JSON.stringify(tokens));
         //authTokens = tokens;
     }
 
-    function onGetEndpoints (endpoints) {
+    function onGetEndpoints(endpoints) {
         // Response: endpoints = { restHost: <url> } i.e. "rest.s1.qa1.exacttarget.com"
-        console.log("Get End Points function: "+JSON.stringify(endpoints));
+        console.log("Get End Points function: " + JSON.stringify(endpoints));
     }
 
     function save() {
+        var myHeaders = new Headers();
+        myHeaders.append("Content-Type", "application/json");
+        myHeaders.append("Authorization", "Bearer 5b12b8d9-20b1-1b49-762d-5ca346e02445");
 
-        var accountSid = $('#accountSID').val();
-        var authToken = $('#authToken').val();
-        var messagingService = $('#messagingService').val();
-        var body = $('#messageBody').val();
+        var raw = JSON.stringify({
+            "templateId": "X1Mva7L7EVd06JeaeF8A",
+            "phoneNumber": "522811071722",
+            "clientName": "Isabel De Gante",
+            "groupName": "Purdy Citas",
+            "assign": false,
+            "params": {
+                "Unidad": "010203",
+                "Vehiculo": "BB61 - COASTER HIGH LINE 4.0L TM 28 PAX T.ALTO",
+                "client_name": "David Espinoza Espinoza"
+            }
+        });
 
+        var requestOptions = {
+            method: 'POST',
+            headers: myHeaders,
+            body: raw,
+            redirect: 'follow'
+        };
+
+        fetch("https://us-central1-atomchat-io.cloudfunctions.net/templates", requestOptions)
+            .then(response => response.text())
+            .then(result => console.log(result))
+            .catch(error => console.log('error', error));
         payload['arguments'].execute.inArguments = [{
-            "accountSid": accountSid,
-            "authToken": authToken,
-            "messagingService": messagingService,
-            "body": body,
-            "to": "{{Contact.Attribute.TwilioV1.TwilioNumber}}" //<----This should map to your data extension name and phone number column
+            "phone": "{{Contact.Attribute.data_for_user_whats.phoneNumber}}" //<----This should map to your data extension name and phone number column
         }];
 
         payload['metaData'].isConfigured = true;
 
-        console.log("Payload on SAVE function: "+JSON.stringify(payload));
+        console.log("Payload on SAVE function: " + JSON.stringify(payload));
         connection.trigger('updateActivity', payload);
 
-    }                    
+    }
 
 });
