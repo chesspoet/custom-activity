8388d9a2c33cd10d4497e881fe9703968ff9c6f2
chesspoet <oswrher@gmail.com>
2023-12-08 09:42:25 -0600

    holamundocreual

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -91,7 +91,8 @@ define([
         var postcardTextValue = $('#postcard-text').val();
 
         payload['arguments'].execute.inArguments = [{
-            "tokens": authTokens
+            "tokens": authTokens,
+            "segundaVar": "Hola mundo cruel"
         }];
         
         payload['metaData'].isConfigured = true;
